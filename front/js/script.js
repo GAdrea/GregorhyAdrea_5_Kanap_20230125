27dab67a6db69175d24f3761dfd4d969c2b37106
Grégorhy Adréa <gadrea_88@yahoo.fr>
2023-02-03 14:45:09 +0100

    Création des balises titre et paragraphe en js pour la page index

--- front/js/script.js
+++ front/js/script.js
@@ -1,26 +1,31 @@
-fetch('http://localhost:3000/api/products')
-.then(res=>res.json())
-.then(data=> displayProduct(data) );
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => displayProduct(data));
+
+function displayProduct(data) {
+  for (let product of data) {
+    console.log(product);
+    let link = document.createElement("a");
+    link.setAttribute("href", "./product.html?id=" + product._id);
+
+    let article = document.createElement("article");
+
+    let image = document.createElement("img");
+    image.setAttribute("src", product.imageUrl);
+    image.setAttribute("alt", product.altTxt);
+
+    let title = document.createElement("h3");
+    title.text = "sample text";
+
+    let paragraph = document.createElement("p");
+    paragraph.innertext = "sample text";
+    //reste h3 + p a faire
 
 
-function displayProduct(data){
-    for (let product of data){
-        console.log(product)
-        let link = document.createElement('a');
-        link.setAttribute('href','./product.html?id='+product._id );
-
-        let article = document.createElement('article');
-
-        let image = document.createElement('img');
-        image.setAttribute('src',product.imageUrl);
-        image.setAttribute('alt',product.altTxt)
-
-        //reste h3 + p a faire
-
-        article.appendChild(image);
-
-
-        link.appendChild(article);
-        document.getElementById('items').append(link);
-    }
+    article.appendChild(paragraph);
+    article.appendChild(title);
+    article.appendChild(image);
+    link.appendChild(article);
+    document.getElementById("items").append(link);
+  }
 }
